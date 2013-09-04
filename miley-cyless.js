621528659199c18eca512d50bf5f578e677c9bfc
Jesse Shawl <jesse@jshawl.com>
2013-09-04 13:50:07 -0400

    mileyCyless hides terms in a list

--- miley-cyless.js
+++ miley-cyless.js
@@ -1,1 +1,22 @@
-alert('working')
+(function(){
+  var mileyCyless = {
+    wordsToCensor : [
+     'Miley Cyrus',
+     'MileyCyrus',
+     'Miley Ray Cyrus'
+    ],
+    init: function(){
+      $.each(this.wordsToCensor, function(i,el){
+        mileyCyless.censor(el);
+      });
+    },
+    censor: function(term){
+      var expression = new RegExp(term,"ig");
+      var matches = $('body').text().match(expression);
+      if (matches){
+        $('body').html($('body').html().replace(expression, "<span class='mc_censored'>"+term+"</span>"));
+      }
+    }
+  }
+  mileyCyless.init();
+})();
